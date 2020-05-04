f743e7de4465fad8e32532d6c60151d8effae779
niallmul97 <niallmul97@gmail.com>
2020-05-04 20:13:30 +0100

    Added weights to votes

--- app/Main.hs
+++ app/Main.hs
@@ -14,14 +14,20 @@ main = do
   print "Candidates: "
   let candidates = getCandidates csvVotes
   -- print $ candidates
-  -- print "Enter number of seats: "
-  -- seats <- readLn :: IO Float
-  -- print $ quota seats votes
+  print "Enter number of seats: "
+  seats <- readLn :: IO Float
+  let quota = findQuota seats votes
+  print "Quota: "
+  print quota
   -- print "Votes: "
   -- print $ votes
   -- print "Split ballots: "
   -- print $ splitBallotHeadTail votes
   -- print "Wack shiz: "
   -- print $ applyCandidatesFirstPrefBallots candidates $ (splitBallotHeadTail votes)
-  print "Candidates first preference count: "
-  print $ getCountFirstPref votes candidates
+  -- print "Candidates first preference count: "
+  -- print $ getCountFirstPref votes candidates
+  print "Candidates above quota: "
+  print $ candidatesAboveQuota quota votes candidates
+  print "Round Winner: "
+  print $ roundWinner quota votes candidates
--- src/Count/CountVotes.hs
+++ src/Count/CountVotes.hs
@@ -1,38 +1,47 @@
 module Count.CountVotes where
-
 import Debug.Trace
 import Data.List
 import Text.Read
 import Data.List.Split (splitOn)
 import Data.Function (on)
 
+weight:: Float
+weight = 1.0
+
+myFst :: (a,b,c) -> a
+myFst (x,_,_) = x
+
+mySnd :: (a,b,c) -> b
+mySnd (_,x,_) = x
+
+myTrd :: (a,b,c) -> c
+myTrd (_,_,x) = x
+
 stringToInt :: String -> Int
 stringToInt string = read string::Int
 
--- intToFloat :: Int -> Float
--- intToFloat x = x::Float
+ballotHeadTail:: [String] -> (String, [String], Float)
+ballotHeadTail (x:xs) = (x, xs, weight)
 
-ballotHeadTail:: [String] -> (String, [String])
-ballotHeadTail (x:xs) = (x, xs)
-
-splitBallotHeadTail:: [[String]] -> [(String, [String])]
+splitBallotHeadTail:: [[String]] -> [(String, [String], Float)]
 splitBallotHeadTail votes = map (ballotHeadTail) votes
 
--- candidatesFirstPrefBallots :: [String] -> [(String, [String])] -> [(String, [String])]
-candidatesFirstPrefBallots ys x = (x, [snd y | y <- ys, fst y == x])
--- (Ed.Balls [ballots] )
+candidatesFirstPrefBallots :: [(String, [String], Float)] -> String -> (String, [([String], Float)])
+candidatesFirstPrefBallots ys x = (x, [(mySnd y, myTrd y) | y <- ys, myFst y == x])
 
-applyCandidatesFirstPrefBallots :: [String] -> [(String, [String])] -> [(String, [[String]])]
-applyCandidatesFirstPrefBallots candidates votes = map (candidatesFirstPrefBallots votes) (candidates)
+applyCandidatesFirstPrefBallots :: [String] -> [(String, [String], Float)] -> [(String, [([String], Float)])]
+applyCandidatesFirstPrefBallots candidates votes = map (candidatesFirstPrefBallots votes) candidates
 
-countFirstPref candidate = (fst candidate, length(snd candidate))
+countFirstPref :: (String, [([String], Float)]) -> (String, Float)
+countFirstPref candidate = (fst candidate, sum $ map (snd) (snd candidate))
 
-getCountFirstPref votes candidates = reverse $ sortBy (compare `on` snd) $ map (countFirstPref) (applyCandidatesFirstPrefBallots candidates (splitBallotHeadTail votes))
+getCountFirstPref ::[[String]] -> [String] -> [(String, Float)]
+getCountFirstPref votes candidates = reverse $ sortBy (compare `on` snd) $ map (countFirstPref) $ applyCandidatesFirstPrefBallots candidates (splitBallotHeadTail votes)
 
--- sortByCount ballot = sortBy (compare `on` snd) ballot
---
--- rankedVotes :: [[String]] -> [String] -> [(String, Int)]
--- rankedVotes votes candidates = map (sortByCount) (getCountFirstPref votes candidates)
+candidatesAboveQuota :: Float -> [[String]] -> [String] -> [(String, Float)]
+candidatesAboveQuota quota votes candidates = filter((>quota) . realToFrac . snd) $ getCountFirstPref votes candidates
+
+roundWinner quota votes candidates = head $ candidatesAboveQuota quota votes candidates
 --get length of second element in each tuple, ie list of first prefs
 --get quota
 --check who was first passed the quota
@@ -43,7 +52,7 @@ getCountFirstPref votes candidates = reverse $ sortBy (compare `on` snd) $ map (
 numberOfValidVotes :: [[String]] -> Int
 numberOfValidVotes votes = length votes
 
-quota :: Float -> [[String]] -> Float
-quota seats votes = 1 + (a / (b + 1))
+findQuota :: Float -> [[String]] -> Float
+findQuota seats votes = 1 + (a / (b + 1))
   where a = fromIntegral $ numberOfValidVotes votes :: Float
         b = seats
